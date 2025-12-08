684073bab42f7cd89121448fc488c2404e1c67d2
Roman Arcturus <roman.arcturus@outlook.com>
2025-12-08 16:41:47 +0100

    so far so good W11

--- src/learn_python/main.py
+++ src/learn_python/main.py
@@ -1,9 +1,12 @@
+from learn_python.phase1_loops import countdown
+
 # from learn_python.functions import add_numbers
 
 # from learn_python.phase12_condition import determine_temperature
 
 if __name__ == "__main__":
     print("\n Program started")
-    # print(add_numbers(3, 4))
+    print("Countdown:")
+    print(countdown(3))
 
     # print(determine_temperature(22))
